bc6f1c601b334e0f309d8b59789c4db802b9e806
Kui-Feng Lee <thinker.li@gmail.com>
2024-01-29 11:08:24 -0800

    net/ipv6: set expires in modify_prefix_route() if RTF_EXPIRES is set.
    Make the decision to set or clean the expires of a route based on the
    RTF_EXPIRES flag, rather than the value of the "expires" argument.
    
    The function inet6_addr_modify() is the only caller of
    modify_prefix_route(), and it passes the RTF_EXPIRES flag and an expiration
    value. The RTF_EXPIRES flag is turned on or off based on the value of
    valid_lft. The RTF_EXPIRES flag is turned on if valid_lft is a finite value
    (not infinite, not 0xffffffff). Even if valid_lft is 0, the RTF_EXPIRES
    flag remains on. The expiration value being passed is equal to the
    valid_lft value if the flag is on. However, if the valid_lft value is
    infinite, the expiration value becomes 0 and the RTF_EXPIRES flag is turned
    off. Despite this, modify_prefix_route() decides to set the expiration
    value if the received expiration value is not zero. This mixing of infinite
    and zero cases creates an inconsistency.
    
    Signed-off-by: Kui-Feng Lee <thinker.li@gmail.com>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -4788,7 +4788,7 @@ static int modify_prefix_route(struct inet6_ifaddr *ifp,
 	} else {
 		table = f6i->fib6_table;
 		spin_lock_bh(&table->tb6_lock);
-		if (!expires) {
+		if (!(flags & RTF_EXPIRES)) {
 			fib6_clean_expires(f6i);
 			fib6_remove_gc_list(f6i);
 		} else {
